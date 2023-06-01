2e68c967970c93b4cfd94ff5b48af4c07371ec3e
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-06-01 15:00:42 +0000

    Editando el navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,12 +5,15 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<Link to="/" style={{textDecoration: "none"}}>
+					<span className="navbar-brand mb-0 h1">TennisTech</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/login">
+						<button className="btn btn-primary">Login</button>
+					</Link>
+					<Link to="/signup">
+						<button className="btn btn-primary">SignUp</button>
 					</Link>
 				</div>
 			</div>
