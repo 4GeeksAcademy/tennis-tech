3e67f3e08a867a8dead2688882f7cd7734e90546
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-06-02 20:57:46 +0000

    Borrador de vista de peril de usuario

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -12,6 +12,7 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Login } from "./pages/login";
 import { Signup } from "./pages/signup";
+import { Profile } from "./pages/profile";
 
 //create your first component
 const Layout = () => {
@@ -30,6 +31,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Login />} path="/login" />
                         <Route element={<Signup />} path="/signup" />
+                        <Route element={<Profile />} path="/profile" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
---
+++ src/front/js/pages/profile.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+export const Profile = () =>{
+    return(
+        <>
+            <h2 className="text-center">Actualiza tu perfil de usuario</h2>
+            <div className="container">
+                <div className="row">
+                    <div className="mb-3">
+                        <label for="formFile" className="form-label">Inserte su foto de perfil</label>
+                        <input className="form-control" type="file" id="formFile"/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                        <div className="input-group mb-3 flex-column">
+                            <label for="formFile" className="form-label">Name: </label>
+                            <input type="text" className="form-control" placeholder="Please insert their name" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="input-group mb-3 flex-column">
+                            <label for="formFile" className="form-label">Lastname: </label>
+                            <input type="text" className="form-control" placeholder="Please insert their lastname" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                        <div className="input-group mb-3 flex-column">
+                            <label for="formFile" className="form-label">Username: </label>
+                            <input type="text" className="form-control" placeholder="Please insert their username" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="input-group mb-3 flex-column">
+                            <label for="formFile" className="form-label">Email: </label>
+                            <input type="text" className="form-control" placeholder="Please insert their email" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="row">
+                    <div className="col-12">
+                        <div className="input-group mb-3 flex-column">
+                            <label for="formFile" className="form-label">Password: </label>
+                            <input type="text" className="form-control" placeholder="Please change their password" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="row">
+                    <div className="col-12 mb-3">
+                    <button className="btn btn-primary" style={{width: "100%"}}>Save your changes</button>
+                    </div>
+
+                </div>
+
+            </div>
+
+        
+        </>
+    )
+}
