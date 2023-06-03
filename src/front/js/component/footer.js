4c4ff7cd4a85c88e198159d806f0ad45abf6ee65
Mariana Arrieche <123425280+maraarrieche@users.noreply.github.com>
2023-06-03 01:28:00 +0000

    acomodando tag de cierre

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,12 +3,9 @@ import "../../styles/footer.css";
 
 
 export const Footer = () => (
-<<<<<<< HEAD
 	<div className="position-absolute fixed-bottom">
-	<footer className="footer mt-auto py-3 text-center" style={{backgroundColor: "#65CC33"}}>
-=======
+	<footer className="footer mt-auto py-3 text-center" style={{backgroundColor: "#65CC33"}}/>
 	<footer className="footer mt-auto py-3 text-center bg-light">
->>>>>>> d2487b49ad4b75906d077dcaffb7cae8a7f6b28d
 		<p>
 			Explore your limits with us © all rights reserved
 		</p>
