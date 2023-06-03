781f01382a1a93cf9e0bae23b07f7d694d8c2b02
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-06-03 01:53:39 +0000

    Editando page home

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,8 +3,7 @@ import "../../styles/footer.css";
 
 
 export const Footer = () => (
-	<div className="position-absolute fixed-bottom">
-	<footer className="footer mt-auto py-3 text-center" style={{backgroundColor: "#65CC33"}}/>
+	<div className=" position-absolute fixed-bottom">
 	<footer className="footer mt-auto py-3 text-center bg-light">
 		<p>
 			Explore your limits with us © all rights reserved
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,20 +7,41 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<>
+			<div className="container">
+				<h1 className="text-center">Canchas</h1>
+				<div className="row my-5">
+					<div className="col-6">
+						<img src="https://civideportes.com.co/wp-content/uploads/2020/08/asphalt-tennis-court-5354328_640.jpg" alt="cancha de cemento azul" style={{maxWidth: "70%"}}/>
+					</div>
+					<div className="col-6">
+						<h4>Cancha de Cemento</h4>
+						<h5>Descripcion: </h5>
+						<p>Esta cancha cuenta con tantos metros..................................................</p>
+					</div>
+				</div>
+				<div className="row my-5">
+					<div className="col-6">
+						<h4>Cancha de Arcilla</h4>
+						<h5>Descripcion: </h5>
+						<p>Esta cancha cuenta con tantos metros..................................................</p>
+					</div>
+					<div className="col-6">
+						<img src="https://i0.wp.com/laverdaddemonagas.com/wp-content/uploads/2022/04/unicas-en-el-oriente-inauguran-canchas-de-tenis-de-arcilla-en-maturin-laverdaddemonagas.com-whatsapp-image-2022-04-13-at-3.40.16-pm-1.jpeg?resize=1080%2C720&ssl=1" alt="cancha de arcilla" style={{maxWidth: "70%"}}/>
+					</div>
+				</div>
+				<div className="row my-5">
+					<div className="col-6">
+						<img src="https://i0.wp.com/matchtenis.com/wp-content/uploads/2019/04/Cesped-3.jpg?w=408&h=306&ssl=1" alt="cancha de grama" style={{width: "70%"}} />
+					</div>
+					<div className="col-6">
+						<h4>Cancha de Grama</h4>
+						<h5>Descripcion: </h5>
+						<p>Esta cancha cuenta con tantos metros..................................................</p>
+					</div>
+				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
+		
+		</>
 	);
 };
