ad3846714fce9211226fc5e18776d1fc9711ddc7
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-06-04 02:41:40 +0000

    Redistribucion de contenido de canchas en componente home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,6 +4,7 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { TeacherCard } from "../component/teacherCard";
 
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
@@ -38,23 +39,23 @@ export const Home = () => {
 				/>
 			</div>
 
-			<div className="container">
+			<div className="container" id="canchas-home">
 
 				<div className="row my-5">
 					<div className="col-6">
 						<img src="https://civideportes.com.co/wp-content/uploads/2020/08/asphalt-tennis-court-5354328_640.jpg" alt="cancha de cemento azul" style={{maxWidth: "70%"}}/>
 					</div>
 					<div className="col-6">
-						<h4>Cancha de Cemento</h4>
+						<h4>Cancha de Concreto</h4>
 						<h5>Descripcion: </h5>
-						<p>Esta cancha cuenta con tantos metros..................................................</p>
+						<p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					</div>
 				</div>
 				<div className="row my-5">
 					<div className="col-6">
 						<h4>Cancha de Arcilla</h4>
 						<h5>Descripcion: </h5>
-						<p>Esta cancha cuenta con tantos metros..................................................</p>
+						<p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					</div>
 					<div className="col-6">
 						<img src="https://i0.wp.com/laverdaddemonagas.com/wp-content/uploads/2022/04/unicas-en-el-oriente-inauguran-canchas-de-tenis-de-arcilla-en-maturin-laverdaddemonagas.com-whatsapp-image-2022-04-13-at-3.40.16-pm-1.jpeg?resize=1080%2C720&ssl=1" alt="cancha de arcilla" style={{maxWidth: "70%"}}/>
@@ -67,7 +68,7 @@ export const Home = () => {
 					<div className="col-6">
 						<h4>Cancha de Grama</h4>
 						<h5>Descripcion: </h5>
-						<p>Esta cancha cuenta con tantos metros..................................................</p>
+						<p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					</div>
 				</div>
 			</div>
