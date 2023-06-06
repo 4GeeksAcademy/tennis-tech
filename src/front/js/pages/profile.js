6a44d4515eb344c696a486b92cf280bcd2cf69c4
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-06-06 20:35:18 +0000

    modificando el input de category en la vista profile

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 export const Profile = () =>{
 
-    const [userProfile, setUserProfile] = useState({})
+    const [profile, setProfile] = useState({})
 
     return(
         <>
@@ -18,13 +18,13 @@ export const Profile = () =>{
                     <div className="col-6">
                         <div className="input-group mb-3 flex-column">
                             <label for="formFile" className="form-label">Name: </label>
-                            <input type="text" onChange={(event) => setUserProfile({...userProfile, name: event.target.value})} value={userProfile.name || ''} className="form-control" placeholder="Please insert their name" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                            <input type="text" onChange={(event) => setProfile({...profile, name: event.target.value})} value={profile.name || ''} className="form-control" placeholder="Please insert their name" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
                         </div>
                     </div>
                     <div className="col-6">
                         <div className="input-group mb-3 flex-column">
                             <label for="formFile" className="form-label">Lastname: </label>
-                            <input type="text" onChange={(event) => setUserProfile({...userProfile, last_name: event.target.value})} value={userProfile.last_name || ''} className="form-control" placeholder="Please insert their lastname" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                            <input type="text" onChange={(event) => setProfile({...profile, last_name: event.target.value})} value={profile.last_name || ''} className="form-control" placeholder="Please insert their lastname" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
                         </div>
                     </div>
 
@@ -33,13 +33,13 @@ export const Profile = () =>{
                     <div className="col-6">
                         <div className="input-group mb-3 flex-column">
                             <label for="formFile" className="form-label">Username: </label>
-                            <input type="text" onChange={(event) => setUserProfile({...userProfile, username: event.target.value})} value={userProfile.username || ''} className="form-control" placeholder="Please insert their username" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                            <input type="text" onChange={(event) => setProfile({...profile, username: event.target.value})} value={profile.username || ''} className="form-control" placeholder="Please insert their username" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
                         </div>
                     </div>
                     <div className="col-6">
                         <div className="input-group mb-3 flex-column">
                             <label for="formFile" className="form-label">Email: </label>
-                            <input type="email" onChange={(event) => setUserProfile({...userProfile, email: event.target.value})} value={userProfile.email || ''} className="form-control" placeholder="Please insert their email" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                            <input type="email" onChange={(event) => setProfile({...profile, email: event.target.value})} value={profile.email || ''} className="form-control" placeholder="Please insert their email" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
                         </div>
                     </div>
 
@@ -47,19 +47,28 @@ export const Profile = () =>{
                     <div className="col-4">
                         <div className="input-group mb-3 flex-column">
                             <label for="formFile" className="form-label">Date of Birth: </label>
-                            <input type="date" onChange={(event) => setUserProfile({...userProfile, date_of_birth: event.target.value})} value={userProfile.date_of_birth || ''} className="form-control" placeholder="Please insert their birthday" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                            <input type="date" onChange={(event) => setProfile({...profile, date_of_birth: event.target.value})} value={profile.date_of_birth || ''} className="form-control" placeholder="Please insert their birthday" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
                         </div>
                     </div>
                     <div className="col-4">
                         <div className="input-group mb-3 flex-column">
                             <label for="formFile" className="form-label">Category: </label>
-                            <input type="text" onChange={(event) => setUserProfile({...userProfile, category: event.target.value})} value={userProfile.category || ''} className="form-control" placeholder="Please insert their category" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                            <select id="inputState" className="form-select" style={{width: "100%"}}>
+                                <option selected>Please select your category</option>
+                                <option>primera</option>
+                                <option>segunda</option>
+                                <option>tercera</option>
+                                <option>cuarta</option>
+                                <option>quinta</option>
+                                <option>sexta</option>
+                            </select>
+                            {/* <input type="text" onChange={(event) => setProfile({...profile, category: event.target.value})} value={profile.category || ''} className="form-control" placeholder="Please insert their category" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/> */}
                         </div>
                     </div>
                     <div className="col-4">
                         <div className="input-group mb-3 flex-column">
                             <label for="formFile" className="form-label">Gender: </label>
-                            <input type="text" onChange={(event) => setUserProfile({...userProfile, gender: event.target.value})} value={userProfile.gender || ''} className="form-control" placeholder="Please insert their gender" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                            <input type="text" onChange={(event) => setProfile({...profile, gender: event.target.value})} value={profile.gender || ''} className="form-control" placeholder="Please insert their gender" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
                         </div>
                     </div>
 
@@ -69,7 +78,7 @@ export const Profile = () =>{
                     <div className="col-12">
                         <div className="input-group mb-3 flex-column">
                             <label for="formFile" className="form-label">Password: </label>
-                            <input type="password" onChange={(event) => setUserProfile({...userProfile, password: event.target.value})} value={userProfile.password || ''} className="form-control" placeholder="Please change their password" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
+                            <input type="password" onChange={(event) => setProfile({...profile, password: event.target.value})} value={profile.password || ''} className="form-control" placeholder="Please change their password" aria-label="Username" aria-describedby="basic-addon1" style={{width: "100%"}}/>
                         </div>
                     </div>
 
--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -3,7 +3,7 @@ import {Context} from "../store/appContext.js"
 
 export const Signup = () => {
 
-    const [user, setuser] = useState({})
+    const [user, setUser] = useState({})
     const [isEqual, setIsEqual] = useState(true)
     const {actions} = useContext(Context)
 
@@ -30,13 +30,13 @@ export const Signup = () => {
                     <h2 className="card-header text-light text-center">Sign Up</h2>
                     <div className="card-body">
                         <div className="input-group mb-3">
-                            <input type="text" onChange={(event) => setuser({...user, username: event.target.value})} value={user.username || ''} className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                            <input type="text" onChange={(event) => setUser({...user, username: event.target.value})} value={user.username || ''} className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
                         </div>
                         <div className="input-group mb-3">
-                            <input type="email" onChange={(event) => setuser({...user, email: event.target.value})} value={user.email || ''} className="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"/>
+                            <input type="email" onChange={(event) => setUser({...user, email: event.target.value})} value={user.email || ''} className="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"/>
                         </div>
                         <div className="input-group mb-3">
-                            <input type="password" onChange={(event) => setuser({...user, password: event.target.value})} value={user.password || ''} className="form-control" id="pass1" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1"/>
+                            <input type="password" onChange={(event) => setUser({...user, password: event.target.value})} value={user.password || ''} className="form-control" id="pass1" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1"/>
                         </div>
                         <div className="input-group mb-3">
                             <input type="password" className="form-control" id="pass2" placeholder="Confirm Password" aria-label="Confirm Password" aria-describedby="basic-addon1"/>
