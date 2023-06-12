216413b49f6ee489a293ec43fce52b4c0d7a0863
Mariana Arrieche <123425280+maraarrieche@users.noreply.github.com>
2023-06-12 00:13:42 +0000

    arreglando un error en el onChange

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -46,10 +46,10 @@ export const Login = () => {
                     <h2 className="card-header text-light text-center">Welcome!!</h2>
                     {(token && token!="" && token!=undefined) ? "You are logged in with this token" + token : <div className="card-body">
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value={username} onChange={() => setUsername(e.target.value)}/>
+                            <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value={username} onChange={e => setUsername(e.target.value)}/>
                         </div>
                         <div className="input-group mb-3">
-                            <input type="password" className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" value={password} onChange={() => setPassword(e.target.value)}/>
+                            <input type="password" className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" value={password} onChange={e => setPassword(e.target.value)}/>
                         </div>
                         <div className="mb-3 text-center">
                             <span style={{color: "white"}}>Forgot your password?</span>
