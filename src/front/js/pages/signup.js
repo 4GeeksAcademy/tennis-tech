26908033028348d41a52fee08229cc9513f3460d
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-06-19 21:35:00 +0000

    Validacion del email en el formulario del signup terminada

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -5,17 +5,28 @@ import {Context} from "../store/appContext.js"
 export const Signup = () => {
 
     const [user, setUser] = useState({})
-    const [isEqual, setIsEqual] = useState(true)
+    const [inputVerified, setinputVerified] = useState(true)
     // const [success, setSuccess] = useState(false)
     const {actions} = useContext(Context)
     const navigate = useNavigate()
 
     const handleAddUser = async () =>{
+        let emailInput = document.getElementById('user-email')
+        let validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+        // if( validEmail.test(emailInput.value) ){
+        //     // alert('Email is valid, continue with form submission');
+        //     setEmailVerified(true)
+        // }else{
+        //     // alert('Email is invalid, skip form submission');
+        //     setEmailVerified(false)
+        // }
         let pass1 = document.getElementById('pass1')
         let pass2 = document.getElementById('pass2')
-        if(pass1.value != pass2.value) setIsEqual(false) // validacion para comprobar que los campos contraseña y confirmar contraseña son iguales
+        if(pass1.value != pass2.value || validEmail.test(emailInput.value) == false) {
+            setinputVerified(false) // validacion para comprobar que los campos contraseña y confirmar contraseña son iguales
+        } 
         else {
-            setIsEqual(true)
+            setinputVerified(true)
             let response = await actions.addNewUser(user)
             if(response){
                 navigate("/login")
@@ -29,7 +40,7 @@ export const Signup = () => {
         <>
             <div className="container position relative">
                 {
-                    !isEqual && <div className="alert alert-danger" role="alert">Las contraseñas no coinciden! Por favor vuelva a intentar</div>
+                    !inputVerified && <div className="alert alert-danger" role="alert">Las contraseñas no coinciden! o el email ingresado es invalido. Por favor vuelva a intentar</div>
                 }
 
                 <div className="card position-absolute top-50 start-50 translate-middle" style={{width: "500px", backgroundColor: "#74B72D"}}>
@@ -39,7 +50,7 @@ export const Signup = () => {
                             <input type="text" onChange={(event) => setUser({...user, username: event.target.value})} value={user.username || ''} className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
                         </div>
                         <div className="input-group mb-3">
-                            <input type="email" onChange={(event) => setUser({...user, email: event.target.value})} value={user.email || ''} className="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"/>
+                            <input type="email" onChange={(event) => setUser({...user, email: event.target.value})} value={user.email || ''} id="user-email" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"/>
                         </div>
                         <div className="input-group mb-3">
                             <input type="password" onChange={(event) => setUser({...user, password: event.target.value})} value={user.password || ''} className="form-control" id="pass1" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1"/>
